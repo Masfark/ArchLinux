128be182d03e942b179148b9e274e6fbfd13966e
Maxim Balakin <Maxim Balakin>
2016-02-27 14:06:17 +0300

    xfce.sh

---
+++ Arch_Install_Sh/xfce.sh
@@ -0,0 +1,5 @@
+sudo pacman -S virtualbox-guest-utils networkmanager network-manager-applet xorg-xinit xorg-twm xterm alsa-utils &&
+sudo systemctl enable vboxservice &&
+sudo systemctl enable NetworkManager &&
+sudo pacman -S xfce4 xfce4-goodies lxdm &&
+sudo nano /etc/lxdm/lxdm.conf
