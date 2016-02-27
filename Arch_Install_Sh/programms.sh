6faaaa21a671c0160548503969968c96e1dbc099
Masfark <masfark@hotmail.com>
2016-02-27 15:49:27 +0300

    programms.sh changed

--- Arch_Install_Sh/programms.sh
+++ Arch_Install_Sh/programms.sh
@@ -1,1 +1,2 @@
-sudo pacman -S gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google rhythmbox ttf-ubuntu-font-family numix-themes yaourt rsync gedit geany firefox ffmpeg flashplugin libvdpau mono
+sudo pacman -S gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google rhythmbox ttf-ubuntu-font-family numix-themes yaourt rsync gedit geany firefox ffmpeg flashplugin libvdpau mono xdg-user-dirs &&
+xdg-user-dirs-update
