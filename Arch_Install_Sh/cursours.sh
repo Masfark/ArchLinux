39de39b39391088d22d4d9ad5c74afbdd3359fd7
masfark <masfark@hotmail.com>
2016-02-28 23:12:47 +0300

    Cursours add

---
+++ Arch_Install_Sh/cursours.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo cp -r ~/archlinux/Cursours/Breeze-Default /usr/share/icons/ &&
+sudo cp -r ~/archlinux/Cursours/oxy-neon /usr/share/icons/ &&
+sudo cp -r ~/archlinux/Cursours/PolarCursorTheme-Blue /usr/share/icons/
