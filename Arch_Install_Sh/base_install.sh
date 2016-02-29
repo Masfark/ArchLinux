836ffefa75a4c35f49d8199ef65db253dceceddc
masfark <masfark@hotmail.com>
2016-02-29 17:02:43 +0300

    base_install.sh

---
+++ Arch_Install_Sh/base_install.sh
@@ -0,0 +1,18 @@
+setfont ruscii_8x16.psfu &&
+mkfs.ext2 /dev/sda1 &&
+mkfs.ext4 /dev/sda2 &&
+mkfs.ext4 /dev/sda4 &&
+mkswap /dev/sda3 &&
+
+mount /dev/sda2 /mnt &&
+mkdir /mnt/boot &&
+mkdir /mnt/home &&
+mount /dev/sda1 /mnt/boot &&
+mount /dev/sda4 /mnt/home &&
+swapon /dev/sda3 &&
+
+nano /etc/pacman.d/mirrorlist &&
+
+pacstrap /mnt base base-devel &&
+genfstab -p /mnt >> /mnt/etc/fstab &&
+arch-chroot /mnt &&
