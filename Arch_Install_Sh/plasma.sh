95b6a8845d2f6c24c666cfaa342b9d35a5721138
masfark <masfark@hotmail.com>
2016-06-11 19:25:25 +0300

    changed plasma.sh

--- Arch_Install_Sh/plasma.sh
+++ Arch_Install_Sh/plasma.sh
@@ -1,11 +1,9 @@
-pacman -S virtualbox-guest-utils plasma plasma-meta sddm networkmanager network-manager-applet xorg-xinit xorg-twm xterm alsa-utils gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google rhythmbox ttf-ubuntu-font-family numix-themes yaourt rsync gedit geany firefox ffmpeg flashplugin libvdpau mono xdg-user-dirs vlc gimp &&
+pacman -S virtualbox-guest-utils plasma sddm networkmanager network-manager-applet xorg-xinit xorg-twm xterm alsa-utils gvfs gvfs-afc gvfs-smb gvfs-gphoto2 gvfs-mtp gvfs-goa gvfs-nfs gvfs-google rhythmbox ttf-ubuntu-font-family numix-themes yaourt rsync gedit geany firefox ffmpeg flashplugin libvdpau mono xdg-user-dirs vlc gimp &&
 
-nano /etc/sddm.conf &&
 systemctl enable vboxservice &&
 systemctl enable NetworkManager &&
-systemctl enable sddm.service &&
-systemctl start sddm &&
+systemctl enable sddm &&
 
 xdg-user-dirs-update &&
 
-cat /archlinux/done.txt
+cat /ArchLinux/done.txt
